e0e5490ce6e8adc1a90fd33c22e72286e75e6c4e
DimitarYordanov17 <ivoj_louie@protonmail.com>
2021-06-26 16:12:19 +0300

    add exercise

---
+++ swap-macro.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define swap(t, x, y) t temp; temp = x; y = x
+
+/*
+void swap(type, int x, int y) {
+    int temp;
+    temp = x;
+    x = y;
+    y = temp;
+    return
+}
+*/
+
+int main () {
+    swap(double, x, y);
+    return 0;
+}
